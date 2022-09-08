8d28f38205221829150ed6cb158f6fa58dfbf8eb
nvctz <arjay.otis@ext.essilor.com>
2022-09-08 17:28:06 +0800

    Goals controller and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middlewares/errorMiddleware');
+const port = process.env.port || 5000;
+
+const app = express();
+
+// JSON body parser
+app.use(express.json());
+// For URL encoded
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+// Override the default express
+// error handler with our own handler.
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+
