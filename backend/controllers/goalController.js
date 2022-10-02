13dde0341dbd3f1824ceeaf34104079889cfc4af
nvctz <arjay.otis@ext.essilor.com>
2022-10-02 11:26:36 +0800

    Finish front-end CRUD

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -49,17 +49,15 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found');
     }
 
-    const user = await User.findById(req.user.id);
-
     // Check if user exists
-    if(!user){
+    if(!req.user){
         res.status(401);
         throw new Error('User not found');
     }
 
     // Check if logged in user
     // matches goal user.
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401);
         throw new Error('User not authorized.');
     }
@@ -84,17 +82,15 @@ const deleteGoal = asyncHandler(async(req, res) => {
         throw new Error('Goal not found');
     }
 
-    const user = await User.findById(req.user.id);
-
     // Check if user exists
-    if(!user){
+    if(!req.user){
         res.status(401);
         throw new Error('User not found');
     }
 
     // Check if logged in user
     // matches goal user.
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401);
         throw new Error('User not authorized.');
     }
---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,17 @@
+
+
+const Spinner = () => {
+
+  return (
+
+    <div className="loadingSpinnerContainer">
+
+        <div className="loadingSpinner"></div>
+
+    </div>
+
+  );
+
+}
+
+export default Spinner;
